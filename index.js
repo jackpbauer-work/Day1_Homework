056f89d4e1979ee9549d4346d9ea0a2338d52a05
Jeryel Blanco <jeryelblanco@gmail.com>
2023-02-21 17:51:47 -0500

    Homework

---
+++ index.js
@@ -0,0 +1,62 @@
+//TODO: Assign corresponding variables
+/////////////////////////////////////////////////////
+let myName = ""
+let myAge = 0
+let myCity = ""
+/////////////////////////////////////////////////////
+const myObj = {
+    name: myName,
+    age: myAge,
+    city: myCity
+}
+
+//TODO: Print the name, age, and city inside myObj using dot 
+//TODO: notation.
+//////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////////////////////////////////
+
+const arrayOfObjects = [
+    {
+        name: "Bob",
+        age: 15,
+        city: "Philadelphia"
+    },
+    {
+        name: "Alex",
+        age: 20,
+        city: "NYC"
+    },
+    {
+        name: "Laura",
+        age: 34,
+        city: "Boston"
+    }
+]
+
+//TODO: Print out Alex's name, age and city
+/////////////////////////////////////////////////////
+
+
+
+
+
+/////////////////////////////////////////////////////
+//TODO: Iterate through arrayOfObjects using a forEach and
+//TODO: print out all names
+/////////////////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////////////////
+//TODO: Iterate through arrayOfOjbects using .map and
+//TODO: increment everyone's age by one
+//! Remember, map needs a return value
+const incrementYear = arrayOfObjects.map(/*code goes here*/)
+console.log(incrementYear)
+
---
+++ readMe.md
@@ -0,0 +1,4 @@
+# Homework
+## Deliverables
+1. Link the javascript file to your HTML using a script tag
+2. Read and complete the TODO's on the index.js
